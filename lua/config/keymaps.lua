2f925f9c10a878a90b2593b0611d8cea94a67c75
George Guimarães <george.guimaraes@gmail.com>
2023-02-27 17:00:01 -0300

    Change buffer command to ,

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,16 +4,16 @@
 --
 local set = vim.keymap.set
 
-set("n", "\\1", "<cmd>BufferLineGoToBuffer 1<cr>", { desc = "Buffer 1" })
-set("n", "\\2", "<cmd>BufferLineGoToBuffer 2<cr>", { desc = "Buffer 2" })
-set("n", "\\3", "<cmd>BufferLineGoToBuffer 3<cr>", { desc = "Buffer 3" })
-set("n", "\\4", "<cmd>BufferLineGoToBuffer 4<cr>", { desc = "Buffer 4" })
-set("n", "\\5", "<cmd>BufferLineGoToBuffer 5<cr>", { desc = "Buffer 5" })
-set("n", "\\6", "<cmd>BufferLineGoToBuffer 6<cr>", { desc = "Buffer 6" })
-set("n", "\\7", "<cmd>BufferLineGoToBuffer 7<cr>", { desc = "Buffer 7" })
-set("n", "\\8", "<cmd>BufferLineGoToBuffer 8<cr>", { desc = "Buffer 8" })
-set("n", "\\9", "<cmd>BufferLineGoToBuffer 9<cr>", { desc = "Buffer 9" })
-set("n", "\\0", "<cmd>BufferLineGoToBuffer 0<cr>", { desc = "Buffer 0" })
+set("n", ",1", "<cmd>BufferLineGoToBuffer 1<cr>", { desc = "Buffer 1" })
+set("n", ",2", "<cmd>BufferLineGoToBuffer 2<cr>", { desc = "Buffer 2" })
+set("n", ",3", "<cmd>BufferLineGoToBuffer 3<cr>", { desc = "Buffer 3" })
+set("n", ",4", "<cmd>BufferLineGoToBuffer 4<cr>", { desc = "Buffer 4" })
+set("n", ",5", "<cmd>BufferLineGoToBuffer 5<cr>", { desc = "Buffer 5" })
+set("n", ",6", "<cmd>BufferLineGoToBuffer 6<cr>", { desc = "Buffer 6" })
+set("n", ",7", "<cmd>BufferLineGoToBuffer 7<cr>", { desc = "Buffer 7" })
+set("n", ",8", "<cmd>BufferLineGoToBuffer 8<cr>", { desc = "Buffer 8" })
+set("n", ",9", "<cmd>BufferLineGoToBuffer 9<cr>", { desc = "Buffer 9" })
+set("n", ",0", "<cmd>BufferLineGoToBuffer 0<cr>", { desc = "Buffer 0" })
 
 set("n", "<leader><tab><tab>", "<cmd>tablast<cr>", { desc = "Last tab" })
 set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New tab" })
@@ -21,7 +21,7 @@ set("n", "<leader>wo", "<cmd>only<cr>", { desc = "Focus window" })
 set("n", "<leader>wq", "<cmd>q<cr>", { desc = "Quit window" })
 
 local picker = require("window-picker")
-set("n", ",", function()
+set("n", "\\", function()
   local picked_window_id = picker.pick_window({
     include_current_win = true,
   }) or vim.api.nvim_get_current_win()
