63b840dfa45433a1a05228f40ddbaec7f5ae546f
George Guimarães <george.guimaraes@gmail.com>
2023-02-28 09:27:04 -0300

    Don't need this as LazyVim got fixed upstream

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-  callback = function()
-    vim.b.miniindentscope_disable = true
-  end,
-})
