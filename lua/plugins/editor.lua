2add2bdd18663609fc553c1523b8e3b1e3be4a4c
George Guimarães <george.guimaraes@gmail.com>
2023-02-28 09:27:40 -0300

    clean up

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -60,20 +60,8 @@ return {
   {
     "echasnovski/mini.move",
     version = false,
-    event = "VeryLazy",
-    options = {
-      mappings = {
-        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-        left = "<M-h>",
-        right = "<M-l>",
-        down = "<M-j>",
-        up = "<M-k>",
-
-        -- Move current line in Normal mode
-        line_left = "<M-h>",
-        line_right = "<M-l>",
-        line_down = "<M-j>",
-        line_up = "<M-k>",
+    lazy = true,
+  },
       },
     },
   },
