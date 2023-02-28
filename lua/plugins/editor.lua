b431dcdd0e40c43dc46c2bf2b30d0d08366c16b6
George Guimarães <george.guimaraes@gmail.com>
2023-02-28 09:28:03 -0300

    change neo-tree ugly git icons

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -62,6 +62,25 @@ return {
     version = false,
     lazy = true,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = "", -- this can only be used in the git_status source
+            renamed = "", -- this can only be used in the git_status source
+            -- Status type
+            untracked = "★",
+            ignored = "◌",
+            unstaged = "✗",
+            staged = "✓",
+            conflict = "",
+          },
+        },
       },
     },
   },
