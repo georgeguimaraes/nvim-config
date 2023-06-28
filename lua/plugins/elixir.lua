ac426b25974204607ca73a3d0b0f28f697fa0376
George Guimarães <george.guimaraes@gmail.com>
2023-06-28 14:10:38 -0300

    Add credo as extension for null-ls

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -29,4 +29,11 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      table.insert(opts.sources, null_ls.builtins.diagnostics.credo)
+    end,
+  },
 }
