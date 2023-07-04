f642bd41027210887ab04940658d93c3fc636d7a
George Guimarães <george.guimaraes@gmail.com>
2023-07-04 11:56:15 -0300

    Updating some defaults from neo-tree and telescope

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,26 +2,10 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      default_component_configs = {
-        git_status = {
-          symbols = {
-            -- Change type
-            added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-            deleted = "", -- this can only be used in the git_status source
-            renamed = "", -- this can only be used in the git_status source
-            -- Status type
-            untracked = "★",
-            ignored = "◌",
-            unstaged = "✗",
-            staged = "✓",
-            conflict = "",
-          },
-        },
-      },
       filesystem = {
-        use_libuv_file_watcher = true,
+        follow_current_file = false,
       },
+      sources = { "filesystem", "buffers", "git_status" },
     },
   },
   {
@@ -30,7 +14,6 @@ return {
       pickers = {
         find_files = {
           hidden = true,
-          no_ignore = true,
           file_ignore_patterns = { ".git" },
         },
       },
