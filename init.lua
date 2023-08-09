1c8005324dd4645321f030b80a6bcfa3b9e22376
George Guimarães <george.guimaraes@gmail.com>
2023-08-09 14:25:30 -0300

    I don't want a fixed nodejs anymore

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.g.neovim_node_host_prog = vim.fn.trim(vim.fn.system("asdf where nodejs 19.6.0")) .. "/bin/node"
+-- vim.g.neovim_node_host_prog = vim.fn.trim(vim.fn.system("asdf where nodejs 19.6.0")) .. "/bin/node"
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
