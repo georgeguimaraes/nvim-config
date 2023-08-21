4c43c17c1d97025c3722eababbf339fb8c8187ab
George Guimarães <george.guimaraes@gmail.com>
2023-08-21 09:59:49 -0300

    Move neogit and octo to git.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,41 @@
 return {
-  "2kabhishek/co-author.nvim",
+  { "2kabhishek/co-author.nvim" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "sindrets/diffview.nvim", -- optional
+    },
+    cmd = "Neogit",
+    config = function()
+      require("neogit").setup({})
+    end,
+  },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "petertriho/cmp-git",
+        config = function()
+          require("cmp_git").setup()
+        end,
+      },
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "git" },
+      }))
+    end,
+  },
 }
--- lua/plugins/neogit.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "TimUntersberger/neogit",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Neogit",
-  },
-}
