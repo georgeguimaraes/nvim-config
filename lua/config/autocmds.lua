b2f874ff32e590013044604a0fba88ed8a3a16cf
George Guimarães <george.guimaraes@gmail.com>
2023-10-23 09:30:48 -0300

    I don't need Elixir ft autocmd anymore

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.cmd([[
-  autocmd BufRead,BufNewFile *.eex set filetype=eex
-  autocmd BufRead,BufNewFile *.html.eex set filetype=heex
-  autocmd BufRead,BufNewFile *.leex set filetype=eex
-]])
