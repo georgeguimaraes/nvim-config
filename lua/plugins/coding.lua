13ee599e826762e5af4b5ea40e0d23b6c24d6c94
George Guimarães <george.guimaraes@gmail.com>
2023-11-06 18:06:34 -0300

    Adds Tabout

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -128,4 +128,28 @@ return {
       },
     },
   },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "abecodes/tabout.nvim" },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return "<Plug>(Tabout)" or (require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next") or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+    },
+  },
+  {
+    "abecodes/tabout.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp",
+    },
+    event = "InsertEnter",
+    config = true,
+  },
 }
