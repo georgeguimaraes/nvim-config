e016418a483a965db33eedb1ef7307b28ebba9ff
George Guimarães <george.guimaraes@gmail.com>
2023-12-13 09:57:46 -0300

    Force Copilot to work on yaml, gitcommit, gitrebase

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        markdown = true,
+        help = true,
+        gitcommit = true,
+        gitrebase = true,
+        yaml = true,
+      },
+    },
+  },
+}
