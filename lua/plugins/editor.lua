1fa1f61fc0275fc0f447520ce1d1641f0ef97c57
George Guimarães <george.guimaraes@gmail.com>
2024-01-07 16:03:03 -0300

    Neo-tree's config for local dev

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,14 +1,22 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    version = false,
+    branch = "main",
+    -- dev = true,
     opts = {
+      filesystem = {
+        follow_current_file = {
+          enabled = false,
+        },
+      },
       -- sources = { "filesystem" },
       window = {
         width = 40,
       },
       -- enable_diagnostics = false,
       -- enable_opened_markers = false,
-      log_level = "trace",
+      log_level = "debug",
       log_to_file = "/tmp/neotree.lazyvim.log",
     },
   },
