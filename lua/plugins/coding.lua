e5ee037567698c826d15c2dfcbc4ccc6f1aba080
George Guimarães <george.guimaraes@gmail.com>
2024-01-07 16:10:47 -0300

    Trying neotab

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -143,12 +143,12 @@ return {
   },
   {
     "L3MON4D3/LuaSnip",
-    dependencies = { "abecodes/tabout.nvim" },
+    dependencies = { "neotab.nvim" },
     keys = {
       {
         "<tab>",
         function()
-          return (require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next") or "<Plug>(Tabout)" or "<tab>"
+          return (require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next") or "<Plug>(neotab-out)" or "<tab>"
         end,
         expr = true,
         silent = true,
@@ -157,15 +157,10 @@ return {
     },
   },
   {
-    "abecodes/tabout.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "hrsh7th/nvim-cmp",
-    },
+    "kawre/neotab.nvim",
     event = "InsertEnter",
     opts = {
       tabkey = "",
-      backwards_tabkey = "",
     },
   },
   {
