c95457fd85fef09dab3dbea23ad12b30ac10da5b
George Guimarães <george.guimaraes@gmail.com>
2024-01-31 19:29:48 -0300

    Add arrow.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -31,4 +31,11 @@ return {
       },
     },
   },
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+    },
+  },
 }
