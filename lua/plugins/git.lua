18623a3202a255b5e8228bb7f5085b3d4c731167
George Guimarães <george.guimaraes@gmail.com>
2024-02-27 10:23:35 -0300

    Add range git log

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -61,6 +61,18 @@ return {
         "<leader>gl",
         function()
           local file = vim.fn.expand("%")
+          vim.cmd([[execute "normal! \<ESC>"]])
+          local line_start = vim.fn.getpos("'<")[2]
+          local line_end = vim.fn.getpos("'>")[2]
+          require("neogit").action("log", "log_current", { "-L" .. line_start .. "," .. line_end .. ":" .. file })()
+        end,
+        desc = "Neogit Log for this range",
+        mode = "v",
+      },
+      {
+        "<leader>gl",
+        function()
+          local file = vim.fn.expand("%")
           require("neogit").action("log", "log_current", { "--", file })()
         end,
         desc = "Neogit Log for this file",
