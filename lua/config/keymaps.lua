2ed42260faa2b3f0de0e6633b35eb6dc38976fad
George Guimarães <george.guimaraes@gmail.com>
2024-04-09 10:16:41 -0300

    Old options that i don't use

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,3 +42,11 @@ end, { desc = "Open commit for this line" })
 set("n", "<leader>gc", function()
   LazyVim.lazygit({ args = { "log" } })
 end, { desc = "Lazygit Commit Log" })
+
+-- set("v", "<leader>gl", function()
+--   local file = vim.fn.expand("%")
+--   vim.cmd([[execute "normal! \<ESC>"]])
+--   local line_start = vim.fn.getpos("'<")[2]
+--   local line_end = vim.fn.getpos("'>")[2]
+--   LazyVim.lazygit({ args = { "log", "-f", "-L" .. line_start .. "," .. line_end .. ":" .. file } })
+-- end, { desc = "Neogit Log for this range" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,8 @@
 -- Add any additional options here
 --
 
-vim.opt.scrolloff = 8
--- vim.opt.timeoutlen = 150
+vim.opt.scrolloff = 2
 vim.opt.swapfile = false
--- vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
 
 if vim.g.neovide then
   vim.opt.guifont = "FiraCodeGG Nerd Font:h16"
