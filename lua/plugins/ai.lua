868aafe81a6df5093adb13329e9ba43b8fa93080
George Guimarães <george.guimaraes@gmail.com>
2024-06-24 12:57:49 -0300

    Add codecompanion.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -11,5 +11,39 @@ return {
       },
     },
   },
-
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+    },
+    opts = function(_, opts)
+      opts.strategies = {
+        chat = "anthropic",
+        inline = "anthropic",
+        tools = "anthropic",
+      }
+      opts.adapters = {
+        anthropic = require("codecompanion.adapters").use("anthropic", {
+          schema = {
+            model = {
+              default = "claude-3-5-sonnet-20240620",
+            },
+          },
+        }),
+      }
+    end,
+    keys = {
+      { "<leader>Aa", "<cmd>CodeCompanionActions<cr>", mode = "n", noremap = true, silent = true },
+      { "<leader>Aa", "<cmd>CodeCompanionActions<cr>", mode = "v", noremap = true, silent = true },
+      { "<leader>AA", "<cmd>CodeCompanionToggle<cr>", mode = "n", noremap = true, silent = true },
+      { "<leader>AA", "<cmd>CodeCompanionToggle<cr>", mode = "v", noremap = true, silent = true },
+      { "<leader>A", "<cmd>CodeCompanionAdd<cr>", mode = "v", noremap = true, silent = true },
+    },
+  },
 }
