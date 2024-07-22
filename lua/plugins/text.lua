a2a02ed5baed36147644f71df76e2bea763873a4
George Guimarães <george.guimaraes@gmail.com>
2024-07-22 09:11:13 -0300

    enable codespell everywhere

--- lua/plugins/text.lua
+++ lua/plugins/text.lua
@@ -4,7 +4,7 @@ return {
     optional = true,
     opts = {
       linters_by_ft = {
-        -- ["*"] = { "codespell" },
+        ["*"] = { "codespell" },
       },
       linters = {
         codespell = {
