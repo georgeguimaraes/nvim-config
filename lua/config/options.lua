137d590d5bcbde4c84dbc867fb83f1e87f1d49a2
George Guimarães <george.guimaraes@gmail.com>
2024-07-25 08:50:14 -0300

    Using my forks for now

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,5 +16,3 @@ end
 vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 
 vim.g.lazyvim_ruby_lsp = "solargraph"
-
-vim.treesitter.language.register("markdown", "livebook")
--- lua/plugins/markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "MeanderingProgrammer/markdown.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.file_types, { "livebook" })
-    end,
-  },
-}
