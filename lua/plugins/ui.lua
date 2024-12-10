3b99e6c366e9de07d43f164511c4d2cc57157549
George Guimarães <george.guimaraes@gmail.com>
2024-12-10 08:58:02 -0300

    ui options

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -25,7 +25,7 @@ return {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "e", desc = "Browse File", action = ":Neotree" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
@@ -41,6 +41,16 @@ return {
     "folke/which-key.nvim",
     opts = {
       preset = "modern",
+    },
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      time_interval = 10,
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.4, -- 0.3      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      hide_target_hack = false, -- true     boolean
     },
   },
 }
