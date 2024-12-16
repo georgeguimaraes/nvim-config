e5b0f74a4a2e35b85ddb778f7346c1983fe25e84
George Guimarães <george.guimaraes@gmail.com>
2024-12-16 16:47:10 -0300

    blink.cmp gets better

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,9 +6,17 @@ return {
         preset = "enter",
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_forwad", "fallback" },
+        ["<Esc>"] = { "cancel", "fallback" },
       },
       completion = {
         menu = {
+          draw = {
+            padding = { 1, 0 },
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+            components = {
+              kind_icon = { width = { fill = true } },
+            },
+          },
           border = "rounded",
           winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
         },
