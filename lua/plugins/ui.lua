1b63be473ab4859049312358a07e844cbcd5c383
George Guimarães <george.guimaraes@gmail.com>
2025-01-14 21:09:08 -0300

    using snacks.nvim picker instead of fzf

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -41,7 +41,7 @@ return {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "e", desc = "Browse File", action = ":Neotree" },
-            { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
