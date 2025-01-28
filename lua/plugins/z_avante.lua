f0fd1cc9a3c6589c0571041befe0c1fbf6e0fc4c
George Guimarães <george.guimaraes@gmail.com>
2025-01-28 09:37:39 -0300

    suggestions for avante

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -2,22 +2,58 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
+    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     dependencies = {
       "stevearc/dressing.nvim",
     },
-    opts = {
-      hints = { enabled = false },
-    },
-    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+    opts = function(_, opts)
+      -- Default configuration
+      opts.hints = { enabled = false }
+
+      -- File selector configuration
+      --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
+      opts.file_selector = {
+        provider = "fzf",
+        provider_opts = {},
+      }
+
+      -- Blink.cmp integration
+      -- LSP score_offset is typically 60
+      opts.providers = {
+        avante_commands = {
+          name = "avante_commands",
+          module = "blink.compat.source",
+          score_offset = 90, -- show at a higher priority than lsp
+          opts = {},
+        },
+        avante_files = {
+          name = "avante_files",
+          module = "blink.compat.source",
+          score_offset = 100, -- show at a higher priority than lsp
+          opts = {},
+        },
+        avante_mentions = {
+          name = "avante_mentions",
+          module = "blink.compat.source",
+          score_offset = 1000, -- show at a higher priority than lsp
+          opts = {},
+        },
+      }
+      opts.compat = {
+        "avante_commands",
+        "avante_mentions",
+        "avante_files",
+      }
+    end,
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     optional = true,
-    opts = function(_, opts)
-      opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
-    end,
     ft = function(_, ft)
       vim.list_extend(ft, { "Avante" })
+    end,
+    opts = function(_, opts)
+      opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
     end,
   },
   {
