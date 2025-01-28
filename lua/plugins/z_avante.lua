74b7b25998f06490fef6006b1464f5c04cd8bc45
George Guimarães <george.guimaraes@gmail.com>
2025-01-28 17:09:20 -0300

    new avante.nvim config

--- lua/plugins/z_avante.lua
+++ lua/plugins/z_avante.lua
@@ -2,50 +2,70 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     dependencies = {
       "stevearc/dressing.nvim",
+      "ibhagwan/fzf-lua",
     },
-    opts = function(_, opts)
+    opts = {
       -- Default configuration
-      opts.hints = { enabled = false }
+      hints = { enabled = false },
+
+      ---@alias AvanteProvider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+      provider = "claude", -- Recommend using Claude
+      auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20241022",
+        temperature = 0,
+        max_tokens = 4096,
+      },
 
       -- File selector configuration
       --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string
-      opts.file_selector = {
-        provider = "fzf",
+      file_selector = {
+        provider = "fzf", -- Avoid native provider issues
         provider_opts = {},
-      }
-
-      -- Blink.cmp integration
-      -- LSP score_offset is typically 60
-      opts.providers = {
-        avante_commands = {
-          name = "avante_commands",
-          module = "blink.compat.source",
-          score_offset = 90, -- show at a higher priority than lsp
-          opts = {},
-        },
-        avante_files = {
-          name = "avante_files",
-          module = "blink.compat.source",
-          score_offset = 100, -- show at a higher priority than lsp
-          opts = {},
-        },
-        avante_mentions = {
-          name = "avante_mentions",
-          module = "blink.compat.source",
-          score_offset = 1000, -- show at a higher priority than lsp
-          opts = {},
-        },
-      }
-      opts.compat = {
-        "avante_commands",
-        "avante_mentions",
-        "avante_files",
-      }
-    end,
+      },
+    },
+    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
   },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "saghen/blink.compat" },
+    lazy = true,
+    opts = {
+      sources = {
+        default = { "avante_commands", "avante_mentions", "avante_files" },
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+        -- LSP score_offset is typically 60
+        providers = {
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90,
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100,
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000,
+            opts = {},
+          },
+        },
+      },
+    },
+  },
+
   {
     "MeanderingProgrammer/render-markdown.nvim",
     optional = true,
