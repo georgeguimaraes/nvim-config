7a6bff3b769e5719ec1fb8b3e720e57a7044a32a
George Guimarães <george.guimaraes@gmail.com>
2025-01-28 09:37:25 -0300

    replacing smart-splits with wezterm-move.nvim

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,10 +1,31 @@
 return {
   {
-    "mrjones2014/smart-splits.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      multiplexer_integration = "wezterm",
+    "letieu/wezterm-move.nvim",
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("wezterm-move").move("h")
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("wezterm-move").move("j")
+        end,
+      },
+      {
+        "<C-k>",
+        function()
+          require("wezterm-move").move("k")
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("wezterm-move").move("l")
+        end,
+      },
     },
   },
 }
