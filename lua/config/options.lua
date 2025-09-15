3ab1d824995916474a2cff4b3956d3f55c94e846
George Guimarães <george.guimaraes@gmail.com>
2025-09-15 10:31:57 -0300

    not using neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,11 +7,6 @@ vim.opt.swapfile = false
 vim.g.deprecation_warnings = false
 vim.opt.relativenumber = false
 
-if vim.g.neovide then
-  vim.opt.guifont = "FiraCodeGG Nerd Font:h16"
-  vim.g.neovide_remember_window_size = true
-end
-
 vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 
 vim.g.lazyvim_ruby_lsp = "solargraph"
