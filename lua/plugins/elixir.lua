dc96286bfd51f4b13ee53523f014856ed89ddc85
George Guimarães <george.guimaraes@gmail.com>
2025-09-15 15:26:51 -0300

    using vendored elixir extra to test expert LSP

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -1,8 +1,58 @@
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = { "elixir", "eelixir", "heex", "surface", "livebook" },
+      root = "mix.exs",
+    })
+  end,
   {
     "nvim-mini/mini.icons",
     opts = function(_)
       vim.cmd([[highlight MiniIconsPurple guifg=#9660EE]])
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "elixir", "heex", "eex" })
+      vim.treesitter.language.register("markdown", "livebook")
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "jfpedroza/neotest-elixir",
+    },
+    opts = {
+      adapters = {
+        ["neotest-elixir"] = {},
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      opts.linters_by_ft = {
+        elixir = { "credo" },
+      }
+
+      opts.linters = {
+        credo = {
+          condition = function(ctx)
+            return vim.fs.find({ ".credo.exs" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
+      }
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    optional = true,
+    ft = function(_, ft)
+      vim.list_extend(ft, { "livebook" })
     end,
   },
 }
