3cf4d3765bff6b0c28d4a640279aec6caac076c4
George Guimarães <george.guimaraes@gmail.com>
2025-09-15 15:07:17 -0300

    refactorex doesn't work with neovim HEAD

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -5,12 +5,4 @@ return {
       vim.cmd([[highlight MiniIconsPurple guifg=#9660EE]])
     end,
   },
-  {
-    "synic/refactorex.nvim",
-    ft = "elixir",
-    opts = {
-      auto_update = true,
-      pin_version = nil,
-    },
-  },
 }
