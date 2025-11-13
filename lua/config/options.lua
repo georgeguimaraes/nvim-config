e91b13eb83dd238a3fc4642026667c26c065db5e
George Guimarães <george.guimaraes@gmail.com>
2025-11-13 14:53:12 -0300

    mini.diff hl colors from delta

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,12 @@ vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
 vim.opt.iskeyword:append("-")
 
 vim.g.ai_cmp = false
+
+-- mini.diff highlight colors from delta (.gitconfig)
+vim.api.nvim_set_hl(0, "MiniDiffSignAdd", { fg = "#399a96" })
+vim.api.nvim_set_hl(0, "MiniDiffSignChange", { fg = "#6183bb" })
+vim.api.nvim_set_hl(0, "MiniDiffSignDelete", { fg = "#b2555b" })
+vim.api.nvim_set_hl(0, "MiniDiffOverAdd", { bg = "#399a96" })
+vim.api.nvim_set_hl(0, "MiniDiffOverDelete", { bg = "#b2555b" })
+vim.api.nvim_set_hl(0, "MiniDiffOverChange", { bg = "#c0caf5" })
+vim.api.nvim_set_hl(0, "MiniDiffOverContext", { fg = "#394b70" })
