645d61efe9200a8ac13b01626e7ad6d610eba8e2
George Guimarães <george.guimaraes@gmail.com>
2025-11-18 16:04:05 -0300

    always start a RPC server

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,14 @@ vim.cmd([[
   autocmd FileType elixir setlocal indentkeys-=0\|>
   autocmd FileType elixir setlocal indentkeys-=\|>
 ]])
+
+-- Start Neovim RPC server for remote file opening (Phoenix LiveReload, etc.)
+-- Check if server is already running (v:servername is set when using --listen flag)
+if vim.v.servername == "" then
+  -- No server running, start one and let Neovim choose the path
+  local socket_path = vim.fn.serverstart()
+  vim.env.NVIM = socket_path
+else
+  -- Server already running (started with --listen flag), use existing
+  vim.env.NVIM = vim.v.servername
+end
