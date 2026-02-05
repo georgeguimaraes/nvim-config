5b79438bcc00850a115527fb415d8adf0d49b264
George Guimarães <george.guimaraes@gmail.com>
2026-02-05 11:10:27 -0800

    use my review.nvim plugin from my dir

---
+++ lua/plugins/review.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    dir = "~/code/nvim-plugins/review.nvim",
+    dependencies = {
+      "esmuellert/codediff.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Review" },
+    keys = {
+      { "<leader>r", "<cmd>Review<cr>", desc = "Review" },
+      { "<leader>R", "<cmd>Review commits<cr>", desc = "Review commits" },
+    },
+    opts = {},
+  },
+}
