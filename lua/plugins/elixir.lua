c785412109dfa05d5106ffc22ca8868e9ba1dfc3
George Guimarães <george.guimaraes@gmail.com>
2026-02-23 13:03:20 -0300

    feat: use expert instead of elixir-ls

--- lua/plugins/elixir.lua
+++ lua/plugins/elixir.lua
@@ -5,4 +5,24 @@ return {
       vim.cmd([[highlight MiniIconsPurple guifg=#9660EE]])
     end,
   },
+  -- Replace elixir-ls with expert in mason
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.tbl_filter(function(pkg)
+        return pkg ~= "elixir-ls"
+      end, opts.ensure_installed or {})
+      table.insert(opts.ensure_installed, "expert")
+    end,
+  },
+  -- Disable elixirls, enable expert
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        elixirls = { enabled = false },
+        expert = {},
+      },
+    },
+  },
 }
