95b8c666b2c577514f9cb77f3d919feaca74d79c
George Guimarães <george.guimaraes@gmail.com>
2026-09-17 10:43:44 -0300

    updates

--- lua/plugins/review.lua
+++ lua/plugins/review.lua
@@ -1,14 +1,20 @@
 return {
   {
-    dir = "~/code/nvim-plugins/review.nvim",
+    "georgeguimaraes/review.nvim",
+    version = "*",
     dependencies = {
       "esmuellert/codediff.nvim",
       "MunifTanjim/nui.nvim",
     },
-    cmd = { "Review" },
+    event = "VeryLazy",
     keys = {
-      { "<leader>rr", "<cmd>Review<cr>", desc = "Review" },
+      { "<leader>rr", "<cmd>Review<cr>", desc = "Review working tree" },
       { "<leader>rc", "<cmd>Review commits<cr>", desc = "Review commits" },
+      { "<leader>rb", "<cmd>Review branch<cr>", desc = "Review branch" },
+      { "<leader>rn", ":Review note<cr>", mode = { "n", "v" }, desc = "Review: note here" },
+      { "<leader>re", "<cmd>Review edit<cr>", desc = "Review: edit comment" },
+      { "<leader>rd", "<cmd>Review delete<cr>", desc = "Review: delete comment" },
+      { "<leader>rx", "<cmd>Review export<cr>", desc = "Review: export" },
     },
     opts = {},
   },
